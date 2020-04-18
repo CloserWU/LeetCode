b4c007bafdf31117d0cec85308e25ba531b2cff2
CloserWU <18969946575@163.com>
2020-04-18 10:07:26 +0800

    SkyLine wrong answer TODO

--- Java/src/com/wushuai/math/GetSkyline.java
+++ Java/src/com/wushuai/math/GetSkyline.java
@@ -73,10 +73,116 @@ public class GetSkyline {
         return res;
     }
 
+    public List<List<Integer>> getSkyline_v2(int[][] buildings) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (buildings.length == 0) {
+            return res;
+        }
+        int[] map = new int[buildings[buildings.length - 1][1] + 1];
+        for (int i = 0; i < buildings.length; i++) {
+            int y = buildings[i][2];
+            int x1 = buildings[i][0];
+            int x2 = buildings[i][1];
+            for (int j = x1; j <= x2; j++) {
+                map[j] = Integer.max(map[j], y);
+            }
+        }
+        for (int i = 1; i < map.length; i++) {
+            if (map[i - 1] < map[i]) {
+                List<Integer> list = new ArrayList<>();
+                list.add(i);
+                list.add(map[i]);
+                res.add(list);
+            } else if (map[i - 1] > map[i]) {
+                List<Integer> list = new ArrayList<>();
+                list.add(i - 1);
+                list.add(map[i]);
+                res.add(list);
+            }
+            if (i == map.length - 1) {
+                List<Integer> list = new ArrayList<>();
+                list.add(i);
+                list.add(0);
+                res.add(list);
+            }
+        }
+        return res;
+    }
+
+    class Pair {
+        int x;
+        int y;
+
+        public Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    /**
+     * wrong
+     * @param buildings
+     * @return
+     */
+    public List<List<Integer>> getSkyline_v3(int[][] buildings) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (buildings.length == 0) {
+            return res;
+        }
+        int right = buildings[0][1];
+        int idx = 0;
+        List<Integer> tmp = new ArrayList<>();
+        tmp.add(buildings[0][0]);
+        tmp.add(buildings[0][2]);
+        res.add(tmp);
+        for (int i = 1; i < buildings.length; i++) {
+            int y = buildings[i][2];
+            int x1 = buildings[i][0];
+            int x2 = buildings[i][1];
+            if (x1 == buildings[idx][0]) {
+                if (y > buildings[idx][2]) {
+                    res.get(res.size() - 1).set(1, y);
+                }
+            } else if (x1 <= right) {
+                if (y > buildings[idx][2]) {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(x1);
+                    list.add(y);
+                    res.add(list);
+                } else if (y < buildings[idx][2]) {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(buildings[idx][1]);
+                    list.add(y);
+                    res.add(list);
+                }
+            } else if (x1 > right){
+                List<Integer> list = new ArrayList<>();
+                list.add(buildings[idx][1]);
+                list.add(0);
+                res.add(list);
+                list = new ArrayList<>();
+                list.add(x1);
+                list.add(y);
+                res.add(list);
+            }
+            if (right <= x2) {
+                idx = i;
+            }
+            right = Integer.max(right, x2);
+        }
+        tmp = new ArrayList<>();
+        tmp.add(right);
+        tmp.add(0);
+        res.add(tmp);
+        return res;
+    }
+
     @Test
     public void test() {
         GetSkyline o = new GetSkyline();
-        List<List<Integer>> skyline = o.getSkyline(new int[][]{{2, 9, 10}, {3, 7, 15}, {5, 12, 12}, {15, 20, 10}, {19, 24, 8}});
+        List<List<Integer>> skyline = o.getSkyline_v3(new int[][]{{2, 9, 10}, {3, 7, 15}, {5, 12, 12}, {15, 20, 10}, {19, 24, 8}});
+        skyline = o.getSkyline_v3(new int[][]{{0,2147483646,2147483647}});
+        skyline = o.getSkyline_v3(new int[][]{{1,2,8},{1,2,10},{2,3,8},{3,5,10},{4,5,8},{7,18,8},{8,10,8}});
         for (int i = 0; i < skyline.size(); i++) {
             List<Integer> list = skyline.get(i);
             for (int j = 0; j < list.size(); j++) {
