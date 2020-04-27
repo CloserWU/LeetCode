c66af70eae42368dc351d2c4e4a70273546623d6
CloserWU <18969946575@163.com>
2020-04-27 15:58:25 +0800

    commit

--- Java/src/com/wushuai/daily/d6.java
+++ Java/src/com/wushuai/daily/d6.java
@@ -24,6 +24,7 @@ public class d6 {
     }
 
     /**
+     * 4.26
      * 23. 合并K个排序链表
      *
      * @param lists K个排序链表
@@ -38,7 +39,7 @@ public class d6 {
         });
         ListNode root = new ListNode(-1);
         ListNode head = root;
-        for (ListNode node: lists) {
+        for (ListNode node : lists) {
             if (node != null) {
                 queue.add(node);
             }
@@ -54,9 +55,47 @@ public class d6 {
         return head.next;
     }
 
+    /**
+     * 4.27
+     * 33. 搜索旋转排序数组
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = (left + right) >>> 1;
+            if ((target <= nums[mid]) ^ (target >= nums[left]) ^ (nums[mid] < nums[left])) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left == right && nums[left] == target ? left : -1;
+    }
+
     @Test
     public void test() {
         d6 o = new d6();
-
+        System.out.println(o.search(new int[]{4, 5, 6, 7, 0, 1, 2}, 6));
+        System.out.println(o.search(new int[]{4, 5, 6, 7, 0, 1, 2}, 5));
+        System.out.println(o.search(new int[]{4, 5, 6, 7, 0, 1, 2}, 4));
+        System.out.println(o.search(new int[]{4, 5, 6, 7, 0, 1, 2}, 1));
+        System.out.println(o.search(new int[]{4, 5, 6, 7, 0, 1, 2}, 0));
+        System.out.println(o.search(new int[]{4, 5, 6, 7, 0, 1, 2}, 2));
+        System.out.println(o.search(new int[]{4, 5, 6, 7, 0, 1, 2}, 7));
+        System.out.println(o.search(new int[]{0, 1, 2, 3, 4, 5, 6, 7}, 0));
+        System.out.println(o.search(new int[]{0, 1, 2, 3, 4, 5, 6, 7}, 8));
+        System.out.println(o.search(new int[]{0, 1, 2, 3, 4, 5, 6, 7}, 6));
+        System.out.println(o.search(new int[]{2,3,4,5,1}, 4));
+        System.out.println(o.search(new int[]{2,3,4,5,1}, 5));
+        System.out.println(o.search(new int[]{2,3,4,5,1}, 1));
+        System.out.println(o.search(new int[]{5, 6, 7, 0, 1, 2, 3}, 6));
+        System.out.println(o.search(new int[]{5, 6, 7, 0, 1, 2, 3}, 5));
+        System.out.println(o.search(new int[]{5, 6, 7, 0, 1, 2, 3}, 3));
+        System.out.println(o.search(new int[]{5, 6, 7, 0, 1, 2, 3}, 0));
+        System.out.println(o.search(new int[]{5, 6, 7, 0, 1, 2, 3}, 2));
     }
 }
