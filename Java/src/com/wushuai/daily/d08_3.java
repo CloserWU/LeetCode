d66db89387ba3fd830374c117ea7f7f587382ff9
CloserWU <18969946575@163.com>
2020-08-31 09:54:16 +0800

    August daily problem done

--- Java/src/com/wushuai/daily/d08_3.java
+++ Java/src/com/wushuai/daily/d08_3.java
@@ -345,6 +345,39 @@ public class d08_3 {
     }
 
 
+    /**
+     * 8.31 841. 钥匙和房间
+     *
+     * @param rooms
+     * @return
+     */
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        if (rooms.size() == 1) {
+            return true;
+        }
+        boolean[] visit = new boolean[rooms.size()];
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        visit[0] = true;
+        while (!stack.isEmpty()) {
+            Integer pop = stack.pop();
+            List<Integer> edges = rooms.get(pop);
+            for (Integer r : edges) {
+                if (!visit[r]) {
+                    stack.push(r);
+                    visit[r] = true;
+                }
+            }
+        }
+        for (boolean b : visit) {
+            if (!b) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
     @Test
     public void test() {
         d08_3 o = new d08_3();
